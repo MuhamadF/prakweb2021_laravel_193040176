5a9974be58f0bbd34bfc46ddd80dcf1380a63fe8
Muhamad Fawwazt <muhamad.fawazt@gmail.com>
2021-10-22 15:34:28 +0700

    Upload video 9 (Database dan seeding)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Muhamad F',
+            'email' => 'muhamad.fawazt@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Jamal',
+            'email' => 'jamal@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programing',
+            'slug' => 'web-programing'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'lorem ipsum dolor sit amet',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur enim quaerat molestias ut porro eius sit ipsa quo sapiente corrupti asperiores fuga dolore in voluptatem inventore, accusamus natus. Eum cumque perferendis id nemo in accusamus incidunt debitis consectetur dicta. Maiores quia itaque assumenda incidunt eum asperiores facilis ducimus enim corporis?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'lorem ipsum dolor sit amet',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur enim quaerat molestias ut porro eius sit ipsa quo sapiente corrupti asperiores fuga dolore in voluptatem inventore, accusamus natus. Eum cumque perferendis id nemo in accusamus incidunt debitis consectetur dicta. Maiores quia itaque assumenda incidunt eum asperiores facilis ducimus enim corporis?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'lorem ipsum dolor sit amet',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur enim quaerat molestias ut porro eius sit ipsa quo sapiente corrupti asperiores fuga dolore in voluptatem inventore, accusamus natus. Eum cumque perferendis id nemo in accusamus incidunt debitis consectetur dicta. Maiores quia itaque assumenda incidunt eum asperiores facilis ducimus enim corporis?',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
