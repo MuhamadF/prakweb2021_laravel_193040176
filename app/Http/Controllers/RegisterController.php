fbf9b1a84d36e749d6e5d02771e96de6a4e0d0b4
Muhamad Fawwazt <muhamad.fawazt@gmail.com>
2021-11-18 14:06:35 +0700

    Upload video 15 (User Registration)

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -11,5 +12,20 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' => 'Register'
         ]);
+    }
+
+    public function store(Request $request) {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:4|max:255|unique:users',
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        $validatedData['password'] = bcrypt($validatedData['password']);
+        User::create($validatedData);
+
+        return redirect('/login')->with('success', 'registration successfull! Please login');
+        
     }
 }
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -4,27 +4,47 @@
 <div class="row justify-content-center">
     <div class="col-md-5">
         <main class="form-registration">
-        <form>
             <h1 class="h3 mb-3 fw-normal">Registration Form</h1>
+            <form action="/register" method="post">
+            @csrf
             <div class="form-floating">
-            <input type="name" class="form-control" id="name" placeholder="name" name="name">
+            <input type="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="name" name="name" required value="{{old('name')}}">
             <label for="name">Name</label>
+            @error('name')
+            <div class="invalid-feedback">
+           {{$message}}
+            </div>
+            @enderror
             </div>
             <div class="form-floating">
-            <input type="username" class="form-control" id="username" placeholder="username" name="username">
+            <input type="username" class="form-control @error('username') is-invalid @enderror" id="username" placeholder="username" name="username" required value="{{old('username')}}">
             <label for="username">Username</label>
+            @error('username')
+            <div class="invalid-feedback">
+           {{$message}}
+            </div>
+            @enderror
             </div>
             <div class="form-floating">
-            <input type="email" class="form-control" id="email" placeholder="name@example.com" name="email">
+            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" name="email" required value="{{old('email')}}">
             <label for="email">Email address</label>
+            @error('email')
+            <div class="invalid-feedback">
+           {{$message}}
+            </div>
+            @enderror
             </div>
             <div class="form-floating">
-            <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+            <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password" name="password" required value="{{old('password')}}">
             <label for="password">Password</label>
+            @error('password')
+            <div class="invalid-feedback">
+           {{$message}}
+            </div>
+            @enderror
             </div>
             <button class="w-100 btn btn-lg btn-dark mt-4" type="submit">Login</button>
         </form>
-        <small class="d-block mt-2 text-center">Not registered? <a href="/register">Register now!</a></small>
         </main>
     </div>
 </div>
